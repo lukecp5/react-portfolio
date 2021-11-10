9d827fd870be058b0e9c06fce76a412e6524deab
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 16:43:57 -0500

    Add a return with content and props to the Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,19 @@ import Navigation from "../Navigation";
 function Header(props) {
 	const { currentTab, setCurrentTab } = props;
 
+	return (
+		<header>
+			<div>
+				<h2>Luke's React Portfolio</h2>
+			</div>
+			<div>
+				<Navigation
+					currentTab={currentTab}
+					setCurrentTab={setCurrentTab}
+				></Navigation>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
