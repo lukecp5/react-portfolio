50b5e59ef53e4205b4a341d9789db46fb25c1d27
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 16:55:08 -0500

    Add conditional rendering that selects which tab to display based on the "currentTab" state variable. The currentTab is about by default and is changed based on the user's interaction with the navigation bar

--- src/App.js
+++ src/App.js
@@ -5,24 +5,36 @@ import { About, Portfolio, Contact, Resume } from "./components";
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [currentTab, setCurrentTab] = useState("about");
+
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
