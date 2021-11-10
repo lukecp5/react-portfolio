7ec4552dcd17b172180aaf5ca3d463fa6047ba6f
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 16:39:28 -0500

    Rework directory structure of application and start building the about me page

---
+++ src/components/About/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function About() {
+	return (
+		<section>
+			<div className="center" id="about">
+				<h1>About</h1>
+			</div>
+
+			{/* Image of me and a paragraph next to the picture built in Bootstrap */}
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<img src="https://i.imgur.com/QQfZKVH.jpg" alt="Me" className="img-fluid rounded-circle" />
+					</div>
+					<div className="col-md-6">
+						<p>
+							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
+							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
+							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
+							About Me Section. About Me Section.
+						</p>
+						<p>
+							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
+							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
+							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
+							About Me Section. About Me Section.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default About;
