b7aaa3196cdacae1175e40bfe15d33d11952b429
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 21:48:31 -0500

    Start building out frame of portfolio tab. Planning on using cards and modals to show users info about my portfolio projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,34 +3,29 @@ import {Container, Row, Col} from "react-bootstrap";
 
 function Portfolio() {
 	return (
-		<section>
-			<div className="center" id="portfolio">
-				<h1>Portfolio</h1>
-			</div>
+		<Container>
+			<Row>
+				<div className="jumbotron jumbotron-fluid">
+					<Container>
+					<h1 className="display-4 text-center">Portfolio</h1>
+					<p className="lead responsive text-center">
+						Here are some of my full stack applications built with React, Node, Express, MongoDB, and Bootstrap.
+					</p>
+					</Container>
+					</div>
+				
+			</Row>
+			<Row>
+				<Col>
 
-			{/* Image of me and a paragraph next to the picture built in Bootstrap */}
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6">
-						<img src={require("../../assets/images/lp-avatar.jpg")} alt="Me" className="img-fluid rounded-circle" />
-					</div>
-					<div className="col-md-6">
-						<p>
-							Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section.
-							Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section.
-							Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section.
-							Portfolio Me Section. Portfolio Me Section.
-						</p>
-						<p>
-							Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section.
-							Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section.
-							Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section. Portfolio Me Section.
-							Portfolio Me Section. Portfolio Me Section.
-						</p>
-					</div>
-				</div>
-			</div>
-		</section>
+				</Col>
+			</Row>
+			<Row>
+				<Col xs={12} sm={6} md={4}></Col>
+				<Col xs={12} sm={6} md={4}></Col>
+				<Col xs={12} sm={6} md={4}></Col>
+			</Row>
+		</Container>
 	);
 }
 
