9a42432bfe14c96ffcc72f7245c02409619c9216
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 21:36:55 -0500

    Add another row and a couple of columns using BS4 components. Set-up the alignment and spacing of the avatar and short description about myself

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,24 +12,28 @@ function About() {
 				</div>
 			</Row>
 			{/* Image of me and a paragraph next to the picture built in Bootstrap */}
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6">
-						<img src={require("../../assets/images/lp-avatar.jpg")} alt="Me" className="img-fluid rounded-circle" />
-					</div>
-					<div className="col-md-6">
-						<p>
-							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
-							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
-							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
-							About Me Section. About Me Section.
-						</p>
-						<p>
-							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
-							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
-							About Me Section. About Me Section. About Me Section. About Me Section. About Me Section.
-							About Me Section. About Me Section.
-						</p>
+			<Row>
+				<Col xs={8} sm={6} md={6} lg={3} className="center mx-auto mb-lg-5 mb-m-4 mb-3">
+					<img
+						src="https://github.com/lukecp5/react-portfolio/blob/main/src/assets/images/lp-avatar-thumb.jpg?raw=true"
+						alt="Me"
+						className="img-fluid rounded-circle"
+					/>
+				</Col>
+
+				<Col xs={12} sm={6} md={6} className="text-center my-auto" id="about">
+					<p>
+						My name is Luke Poirrier, and I currently reside in North Georgia. I have over 10 years of
+						experience in various types of coding. I am currently enrolled at a coding boot camp at the
+						Georgia Institute of Technology(GA Tech) to solidify my foundational knowledge of development.
+					</p>
+					<p>
+						I have experience in front-end web development, working with technologies like AJAX, Bootstrap,
+						CSS, HTML, JavaScript, JSON, Materialize.css, Node.JS, and various other modern development
+						media.
+					</p>
+				</Col>
+			</Row>
 		</Container>
 	);
 }
