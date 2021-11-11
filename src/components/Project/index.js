93e47d1111aba795affcf9243504d2ad03257660
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-11 15:25:01 -0500

    Rework the Project component to be more modular and scalable

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,17 +5,17 @@ import Button from 'react-bootstrap/Button';
 const Project = (props) => {
       const project = props.project;
       return (
-                  <Card key={props.key}>
-                        <Card.Img variant="top" src={project.image} alt={project.name} className="img-fluid rounded-circle"/>
+                  <Card>
+                        <Card.Img variant="top" src={require('./' + project.image).default} alt={project.name} className="img-fluid rounded-circle"/>
                         <Card.Body className="mx-auto">
-                              <Card.Title>{project.name}</Card.Title>
+                              <Card.Title>{project.name.toString()}</Card.Title>
                               {/* <Card.Text>
                                     {project.description}
                               </Card.Text> */}
                         </Card.Body>
                         <Card.Footer className="m-auto">
-                              <Button variant="primary" href={project.live}>Live Demo</Button>
-                              <Button variant="secondary" href={project.github}>GitHub Repo</Button>
+                              <Button variant="primary p-1 p-sm-2" href={project.live}>Live Demo</Button>
+                              <Button variant="secondary p-1 p-sm-2" href={project.github}>GitHub Repo</Button>
                         </Card.Footer>
                   </Card>   
                  )}
