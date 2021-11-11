199a0dab7aaf64c063ca3e53ec22cfb4d1b2ea25
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 21:42:25 -0500

    Refactor the header component using Bootstrap styling to create a responsive, attractive heading and navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,20 +4,25 @@ import {Container, Row, Col} from "react-bootstrap";
 import "./header.css";
 
 function Header(props) {
-	const { currentTab, setCurrentTab } = props;
+	const {currentTab, setCurrentTab} = props;
 
 	return (
-		<header>
-			<div>
-				<h2>Luke's React Portfolio</h2>
-			</div>
-			<div>
-				<Navigation
-					currentTab={currentTab}
-					setCurrentTab={setCurrentTab}
-				></Navigation>
-			</div>
-		</header>
+		<Container fluid className="p-0">
+			<Row className="p-0">
+				<Col className="text-center p-0" md={12}>
+				<div className="jumbotron-fluid mt-2">
+					<h2 className="display-2 mb-0 pb-0">Luke Poirrier</h2>
+					<p className="lead pt-0 mt-0">Full Stack Developer</p>
+					</div>
+				</Col>
+				<header>
+					<div></div>
+					<div>
+						<Navigation currentTab={currentTab} setCurrentTab={setCurrentTab}></Navigation>
+					</div>
+				</header>
+			</Row>
+		</Container>
 	);
 }
 
