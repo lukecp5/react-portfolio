8cd3027e4564a1df8189cd3a423937f5cb4763e0
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 21:32:34 -0500

    Imported the remaining components to App.js. Import useState from react. Import Container, Row, and Col from react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useState } from 'react';
+import React, {useState} from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Container, Row, Col} from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "./App.css";
 
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
 
 library.add(fab);
-
 
 function App() {
 	const [currentTab, setCurrentTab] = useState("about");
