f33cc3be41e8da49714736fe3a9933a377538785
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-11 11:36:18 -0500

    Set-up a map function to map each of my portfolio projects to a card element with their respective names, images, and links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col, CardGroup } from "react-bootstrap";
+import portfolioProjects from "../../projects";
+import Project from "../Project";
 
 function Portfolio() {
 	return (
@@ -7,24 +9,35 @@ function Portfolio() {
 			<Row>
 				<div className="jumbotron jumbotron-fluid">
 					<Container>
-					<h1 className="display-4 text-center">Portfolio</h1>
-					<p className="lead responsive text-center">
-						Here are some of my full stack applications built with React, Node, Express, MongoDB, and Bootstrap.
-					</p>
+						<h1 className="display-4 text-center">Portfolio</h1>
+						<p className="lead responsive text-center">
+							Here are some of my full stack applications built with React, Node, Express, MongoDB, and Bootstrap.
+						</p>
 					</Container>
-					</div>
-				
+				</div>
 			</Row>
-			<Row>
-				<Col>
-
-				</Col>
-			</Row>
-			<Row>
-				<Col xs={12} sm={6} md={4}></Col>
-				<Col xs={12} sm={6} md={4}></Col>
-				<Col xs={12} sm={6} md={4}></Col>
-			</Row>
+			<Container fluid="md">
+				<CardGroup>
+					<Row className="mb-md-5">
+					{portfolioProjects.filter((item, idx) => idx < 3).map((project, index) => {
+						return (
+							<Col key={index} md={4}>
+							<Project project={project} key={index} />
+							</Col>
+						)
+					})}
+					</Row>
+					<Row>
+					{portfolioProjects.filter((item, idx) => idx > 2).map((project, index) => {
+						return (
+							<Col key={index} md={4}>
+							<Project project={project} key={index} />
+							</Col>
+						)
+					})}
+					</Row>
+				</CardGroup>
+			</Container>
 		</Container>
 	);
 }
