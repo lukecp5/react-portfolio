df683dfc15f9af5be67f642cb99c5b03f20950c7
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 21:43:34 -0500

    Import Nav and Navbar from BS4

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+// import { Collapse } from "react-bootstrap/Navbar";
+import {Container} from "react-bootstrap";
+import {Nav} from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
