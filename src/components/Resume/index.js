fd6b02b27bd066ef2c6c3675425ee7543f6c5c1d
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-10 23:22:31 -0500

    Set-up imports for Resume Component and build an array of objects holding my various skills to be used in an accordion.

---
+++ src/components/Resume/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container, Row, Col, Accordion, ListGroup } from "react-bootstrap";
+import AccordionHeader from "react-bootstrap/esm/AccordionHeader";
+
+/*//----------------------------------------------------------------------------+
+// - Front-End Proficiencies
+// _ HTML5, CSS3, JavaScript, jQuery, Bootstrap, React, version control/Git, responsive design
+// - Dev Tool Proficiencies 
+// _ Git npm Jest Webpack
+// - Database Proficiencies
+// _ MySQL, Sequelize, NoSQL, SQLite, GraphQL, MongoDB, Mongoose */
+const skillTypes = [
+	{
+		name: "Frontend Development",
+		skills: [
+			"HTML5",
+			"CSS3",
+			"JavaScript",
+			"React",
+			"Redux",
+			"Bootstrap",
+			"Version Control/Git",
+			"Responsive Design"
+		]
+	}, {
+		name: "Development Tools",
+		skills: [
+			"Git",
+			"npm",
+			"Jest",
+			"Webpack"
+		]
+	}, {
+		name: "Database Skills",
+		skills: [
+			"MySQL",
+			"Sequelize",
+			"NoSQL",
+			"SQLite",
+			"GraphQL",
+			"MongoDB",
+			"Mongoose"
+		]
+	},
+];
